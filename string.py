ccad31f36b38cff9e99ffa5e3ca52651aabf631e
Dinesh Katupputhur Ramprasath <dineshkvr@gmail.com>
2018-07-07 10:29:38 -0700

    added string python file

---
+++ string.py
@@ -0,0 +1,9 @@
+name = "Reflections Circle"
+print name ;
+print "length of string is :",len(name) ;
+x = len(name)
+for i in range(0,x,2):
+  print name[i];
+
+for i in range(1,x,2):
+   print name[i];

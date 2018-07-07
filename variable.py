223a086d6cdb88cdb75cb61aad36b82c4201a8df
Dinesh Katupputhur Ramprasath <dineshkvr@gmail.com>
2018-07-07 09:39:54 -0700

    added python code

---
+++ variable.py
@@ -0,0 +1,21 @@
+
+a = 5;
+print "interger is:",a;
+
+b = 1.3232;
+print "float is: ",b;
+
+
+name  = 'dinesh'
+print "name is : ",name ;
+
+
+
+print "character of name[1] is: ",name[1];
+
+list = [1,2,3,4];
+print "list is : ",list;
+
+
+dictionary = { 'name' : 'dinesh' , 'age' : '25' }
+print "dictionary is : " ,dictionary;
